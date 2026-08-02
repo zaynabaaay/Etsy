30df52bb1b4c45beb1d2404441f733a425bdea37
Zaynab Omer <omer0014@algonquinlive.com>
2026-08-02 16:35:41 -0400

    Preload invitation transition

--- green-sage-wedding-invite/envelope.js
+++ green-sage-wedding-invite/envelope.js
@@ -47,7 +47,7 @@
     window.clearTimeout(openingFallback);
     window.clearTimeout(flapFallback);
     window.clearTimeout(cardStageFallback);
-    window.location.assign('invitation.html');
+    window.location.assign('invitation.html?v=20260802-seamless-invite');
   };
 
   const runCardStage = (className, animationName, fallbackDelay, nextStage) => {
