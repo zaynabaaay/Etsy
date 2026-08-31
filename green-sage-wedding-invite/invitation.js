ebb0a4ae12f32ddd1e2bc055817a81cb9de3f2ff
Zaynab Omer <omer0014@algonquinlive.com>
2026-08-31 04:15:23 -0400

    Tighten invitation hero name stack

--- green-sage-wedding-invite/invitation.js
+++ green-sage-wedding-invite/invitation.js
@@ -21,7 +21,7 @@
 
   const typographyStyles = document.createElement('link');
   typographyStyles.rel = 'stylesheet';
-  typographyStyles.href = 'typography-system.css?v=20260831-hero-intro-v1';
+  typographyStyles.href = 'typography-system.css?v=20260831-name-stack-v1';
   document.head.appendChild(typographyStyles);
 
   const scheduleList = document.querySelector('.schedule-list');
